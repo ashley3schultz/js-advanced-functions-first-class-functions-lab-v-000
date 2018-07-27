f1385c18f8fa8f6ab8fcd442be6ca50b2fd524f2
Ashley Schultz <ashleyeschultz@gmail.com>
2018-07-27 22:10:04 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,7 +1,5 @@
 // Code your solution in this file!
 function returnFirstTwoDrivers(drivers) {
-  const arr = function () {
-    drivers.slice(0, 2);
-  }
+  const arr = drivers.slice(0, 2);
   return arr
 }
