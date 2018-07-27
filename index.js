c783c22c74165ebe3a0ae94d5d15e0ac15c472ae
Ashley Schultz <ashleyeschultz@gmail.com>
2018-07-27 22:18:58 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -3,6 +3,6 @@ const returnFirstTwoDrivers = function (drivers) {
   return drivers.slice(0, 2);
 }
 
-const returnLastTwoDrivers() = function (drivers) {
+const returnLastTwoDrivers = function (drivers) {
   return drivers.slice(-3, -1);
 }
