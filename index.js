f06a717866f6f9a01aa3a81c8d532dd9953d8998
Ashley Schultz <ashleyeschultz@gmail.com>
2018-07-27 22:33:20 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -20,6 +20,6 @@ const fareDoubler = createFareMultiplier(2)
 
 const fareTripler = createFareMultiplier(3)
 
-function selectDifferentDrivers(arr, fn) {
-  return fn(arr)
+function selectDifferentDrivers(arr, func) {
+  return func(arr)
 }
