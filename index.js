a3da46c727a91d7d141440726e7e270c64a0eda7
Ashley Schultz <ashleyeschultz@gmail.com>
2018-07-27 22:25:47 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -8,3 +8,8 @@ const returnLastTwoDrivers = function (drivers) {
 }
 
 const selectingDrivers = [returnFirstTwoDrivers, returnLastTwoDrivers]
+
+
+function createFareMultiplier(fn) {
+  return fn
+}
